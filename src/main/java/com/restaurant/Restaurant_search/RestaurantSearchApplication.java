8d90750ed478a498f8655126ae2860a21223ae93
InseoungYoon <isyoon12@gwnu.ac.kr>
2024-07-06 17:25:31 +0900

    first commit

---
+++ src/main/java/com/restaurant/Restaurant_search/RestaurantSearchApplication.java
@@ -0,0 +1,14 @@
+package com.restaurant.Restaurant_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantSearchApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RestaurantSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restaurant/Restaurant_search/RestaurantSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.Restaurant_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
