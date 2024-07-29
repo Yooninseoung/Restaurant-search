6cf0dd38aebfa3f854d11cde6a6e1cfd968511fd
Yooninseoung <isyoon2000@naver.com>
2024-07-29 22:33:01 +0900

    랜덤 메뉴 추천 기능

--- src/main/java/com/restaurant/Restaurant_search/controller/BasicController.java
+++ src/main/java/com/restaurant/Restaurant_search/controller/BasicController.java
@@ -1,6 +1,8 @@
 package com.restaurant.Restaurant_search.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,6 +10,25 @@ public class BasicController {
     @RequestMapping("/index")
     public String home(){ // 홈페이지 반환
         return "index";
+    }
+
+    @GetMapping("/recommendMenu")
+    public String recommendMenu(Model model){
+        String [] menu =  {
+                "김치 찌개", "된장 찌개", "비빔밥", "불고기", "삼겹살",
+                "떡볶이", "김밥", "치킨", "갈비탕", "설렁탕",
+                "칼국수", "냉면", "제육 볶음", "감자탕", "부대 찌개",
+                "해물 파전", "닭갈비", "양념 치킨", "돼지 갈비", "순대 국밥",
+                "닭발", "마라탕", "닭 볶음탕", "찜닭", "아구찜",
+                "간장 게장", "탕후루", "핫도그", "피자", "해물탕"
+        };
+
+        int idx = (int)(Math.random()*30);
+
+        model.addAttribute("menu", menu[idx]);
+
+
+        return "restaurant/recommendFood";
     }
 
 }
